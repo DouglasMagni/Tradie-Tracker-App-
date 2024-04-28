451055c4d470a9f81a6024c08270e677d7c4b88d
douglas <douglas_post93@hotmail.com>
2024-04-29 10:25:36 +1200

    auth0 working

--- client/index.tsx
+++ client/index.tsx
@@ -1,14 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import routes from './routes.tsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 
-const router = createBrowserRouter(routes)
 const queryClient = new QueryClient()
-
+const router = createBrowserRouter(routes)
+const onRedirectCallback = (appState: any) => {
+  // Redirect to the welcome page after successful login
+  router.navigate('/welcomePage')
+}
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     /**
@@ -18,8 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
       domain="dev-dlndszbceh0vaq63.au.auth0.com"
       clientId="Iinvdb3OkUVh4IEEaSNsB9MJa9GcwRdh"
       authorizationParams={{
-        redirect_uri: window.location.origin,
         audience: 'https://dev-dlndszbceh0vaq63.au.auth0.com/api/v2/',
+        redirect_uri: window.location.origin,
+        onRedirectCallback: { onRedirectCallback },
       }}
     >
       <QueryClientProvider client={queryClient}>
